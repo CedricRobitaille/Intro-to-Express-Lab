af6a9aa6066b33d3a8a6b3be6c4ee3aad09680c9
Cedric <crubbertire@rogers.com>
2025-10-16 14:06:11 -0400

    Roll Page with Fall-backs

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const app = express();
 
-app.get("greetings/:userParams", (req, res) => {
+app.get("/greetings/:userParams", (req, res) => {
   res.send(
-    `Hello ${req.params.userParams}`
+    `Hello ${req.params.userParams}, Long time no see!`
   ) 
 });
 
 app.get("/greetings", (req,res) => {
-  res.send("Heya!")
+  res.send("Hi there,\nI don't believe we've met!")
 })
+
+
+app.get("/roll/:numParams", (req, res) => {
+  let responseMsg = "";
+  const params = Number(req.params.numParams);
+  console.log(params)
+  if (!Number.isInteger(params)) {
+    responseMsg = `Sorry, but we need a number... ${req.params.numParams} isn't very number-like :sigh:`
+  } else {
+    const randomNum = parseInt(Math.random()*parseFloat(req.params.numParams));
+    responseMsg = `You spinned a ${randomNum}`;
+  }
+  res.send(responseMsg)
+})
+
+app.get("/roll", (req, res) => {
+  res.send("Please input a number in the url!")
+})
+
+
+
+
+
+
+
 
 app.listen(3000, ()=> {
   console.log("Listening on port 3000");
